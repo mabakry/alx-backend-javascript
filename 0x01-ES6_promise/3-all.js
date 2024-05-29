69588e45395f9ac01e2efbd8ec07fa654924264b
mabakry <mabakry@gmail.com>
2024-05-29 21:32:21 +0000

    3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,10 @@
-import { uploadPhoto, createUser } from './utils.js';
-export function handleProfileSignup() {
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((resolve) => {
-      console.log('${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}');
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .cath(() => console.log('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
