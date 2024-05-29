3527b3eaddea5c3efe3c07339fe181c3e406a880
mabakry <mabakry@gmail.com>
2024-05-29 04:11:03 +0000

    1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,12 @@
 export default getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success = true){
-      resolve(status: 200, body: 'Success')
+    if (success) {
+      resolve({
+        status: 200,
+	body: 'Success',
+      });
     } else {
-      reject(Error("The fake API is not working currently"))}
-  }
-  );
+      reject(new Error("The fake API is not working currently"))
+    }
+  });
 }
