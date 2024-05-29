881401413dbaf5b584e40815a1fc730af008221f
mabakry <mabakry@gmail.com>
2024-05-29 22:45:52 +0000

    10

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,10 +2,11 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   let res = {};
+
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    res = { photo, user};
+    res = { photo, user };
   } catch (err) {
     res = { photo: null, user: null };
   }
