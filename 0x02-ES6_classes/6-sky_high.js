fd023bc3bcddbe844b8a84af5df05ec2cb70b514
mabakry <mabakry@gmail.com>
2024-05-30 23:19:21 +0000

    6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,21 +1,21 @@
 import Building from './5-building';
 
-export class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
-  }
-
-  get sqft() {
-    return this._sqft;
   }
 
   get floors() {
     return this._floors;
   }
 
+  set floors(value) {
+    this._floors = value;
+  }
+
   evacuationWarningMessage() {
-    return 'Evacuate slowly the NUMBER_OF_FLOORS floors';
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 
 }
