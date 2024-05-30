189d09dd81698262121ca4ae8073ba8651e76e05
mabakry <mabakry@gmail.com>
2024-05-30 20:42:44 +0000

    0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,11 @@
+/**
+ * A class room.
+ */
+
 export default class ClassRoom {
+	/**
+	 * creats a new class room
+	 */
 	constructor(maxStudentsSize) {
 		this._maxStudentsSize = maxStudentsSize;
 	}
